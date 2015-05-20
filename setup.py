b1567a45860016f951d943b75e352015f7371d56
Nicolas Jeannerod <nicolas.jeannerod@ens.fr>
2015-05-20 10:45:50 -0400

    make repo installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "FrontIRCBot",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['irc'],
+    author = "Niols",
+    author_email = "niols@niols.net",
+    description = "",
+    license = "",
+    keywords = "front irc bot",
+    url = "https://github.com/Niols/FrontIRCBot/",
+)
