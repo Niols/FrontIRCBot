859019c15fceb30734299bd87b3d3deb264fca83
Niols <niols@niols.net>
2015-05-20 23:57:54 +0200

    add tests for defaults and debuged defaults

--- frontircbot/defaults.py
+++ frontircbot/defaults.py
@@ -5,11 +5,11 @@ from __future__ import unicode_literals
 
 
 
-DEFAULT_PORT = 6667
+PORT = 6667
 "Default port for IRC servers."
 
-DEFAULT_TIME_BETWEEN_MESSAGES = 1
+TIME_BETWEEN_MESSAGES = 1
 "Default number of seconds between each message sent to a target."
 
-DEFAULT_NICKNAME = 'FrontIRCBot'
+NICKNAME = 'FrontIRCBot'
 "Default name of the bot."
